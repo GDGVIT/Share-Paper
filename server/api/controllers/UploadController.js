03589269ce3201beae948145a7a9847e0d349054
Anurag Tiwari <tiwari.anurag126@gmail.com>
2017-01-26 15:01:26 +0530

    API base64 temp change

--- server/api/controllers/UploadController.js
+++ server/api/controllers/UploadController.js
@@ -7,52 +7,59 @@
 
 module.exports = {
 	'upload' : function(req, res) {
-		if(req.param('c_cd') && req.param('slot') && req.param('no_of_images') && req.param('img_arr') && req.param('sem') && req.param('year')) {
-			var c_cd = req.param('c_cd');
-			var slot = req.param('slot');
-			var no_of_images = req.param('no_of_images');
-			var img_arr = req.param('img_arr');
-			var sem = req.param('sem');
-			var year = req.param('year');
-			console.log( "c_cd =" + c_cd + "\nslot = " + slot + "\nno_of_images = " + no_of_images + "\nimg_arr = " + img_arr);
-			Upload.create({
-				'c_cd' : c_cd,
-				'slot' : slot,
-				'no_of_images' : no_of_images,
-				'img_arr' : img_arr,
-				'sem' : sem,
-				'year' : year
-			}, function uploadedPaper(err, paper) {
-					if(err) {
-						var reply = {
-							'status' : 100,
-							'message' : 'An error occured while uploading the paper' 
-						};
-						res.status(200).json(reply);
-						return;
-					} else {
-						var token = req.session.id;
-						var reply = {
-							'status' : 101,
-							'message' : 'Successfully uploaded',
-							'c_cd' : paper.c_cd,
-							'slot' : paper.slot,
-							'no_of_images' : paper.no_of_images,
-							'img_arr' : img_arr,
-							'sem' : sem,
-							'year' : year
-						};
-						res.status(200).json(reply);
-						return;
-					}
-				});
-		} else {
-			var reply = {
-				'status' : 102,
-				'message' : 'All the parameters were not received'
-			};
-			res.status(200).json(reply);
-		}
+		var image = req.param('image');
+		console.log(image);
+		var reply = {
+			'status' : 100,
+			'message' : 'Sent'
+		};
+		res.status(200).json(reply);
+		// if(req.param('c_cd') && req.param('slot') && req.param('no_of_images') && req.param('img_arr') && req.param('sem') && req.param('year')) {
+		// 	var c_cd = req.param('c_cd');
+		// 	var slot = req.param('slot');
+		// 	var no_of_images = req.param('no_of_images');
+		// 	var img_arr = req.param('img_arr');
+		// 	var sem = req.param('sem');
+		// 	var year = req.param('year');
+		// 	console.log( "c_cd =" + c_cd + "\nslot = " + slot + "\nno_of_images = " + no_of_images + "\nimg_arr = " + img_arr);
+		// 	Upload.create({
+		// 		'c_cd' : c_cd,
+		// 		'slot' : slot,
+		// 		'no_of_images' : no_of_images,
+		// 		'img_arr' : img_arr,
+		// 		'sem' : sem,
+		// 		'year' : year
+		// 	}, function uploadedPaper(err, paper) {
+		// 			if(err) {
+		// 				var reply = {
+		// 					'status' : 100,
+		// 					'message' : 'An error occured while uploading the paper' 
+		// 				};
+		// 				res.status(200).json(reply);
+		// 				return;
+		// 			} else {
+		// 				var token = req.session.id;
+		// 				var reply = {
+		// 					'status' : 101,
+		// 					'message' : 'Successfully uploaded',
+		// 					'c_cd' : paper.c_cd,
+		// 					'slot' : paper.slot,
+		// 					'no_of_images' : paper.no_of_images,
+		// 					'img_arr' : img_arr,
+		// 					'sem' : sem,
+		// 					'year' : year
+		// 				};
+		// 				res.status(200).json(reply);
+		// 				return;
+		// 			}
+		// 		});
+		// } else {
+		// 	var reply = {
+		// 		'status' : 102,
+		// 		'message' : 'All the parameters were not received'
+		// 	};
+		// 	res.status(200).json(reply);
+		// }
 	},
 
 	'view' : function(req, res) {
